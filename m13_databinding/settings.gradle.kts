9a7fdd36ebacf72e00a5092bd47f9f69ad1b15ad
tatiana_sht <tatianaorlova0271@gmail.com>
2024-12-20 12:16:30 +1000

    Added solution homework 13

---
+++ m13_databinding/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My_Android_Dev_HW_13"
+include(":app")
+ 
